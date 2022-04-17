169e5d6f3b09184c429402efcfe6cbe242bbf8c0
Garvit Joshi <garvitjoshi9@gmail.com>
2022-04-17 14:46:51 +0530

    Added Project Structure

---
+++ src/test/java/com/awscclpu/mailingservice/MailingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awscclpu.mailingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
