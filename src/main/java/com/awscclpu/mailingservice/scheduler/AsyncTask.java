9455ba6552950aa4af23f22cb9f208a761c17566
Garvit Joshi <garvitjoshi9@gmail.com>
2022-05-15 12:34:48 +0530

    Added streams instead of for loops

--- src/main/java/com/awscclpu/mailingservice/scheduler/AsyncTask.java
+++ src/main/java/com/awscclpu/mailingservice/scheduler/AsyncTask.java
@@ -5,6 +5,7 @@ import com.awscclpu.mailingservice.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @Component
 @Slf4j
+@EnableAsync
 public class AsyncTask {
 
 	final UserRepository userRepository;
@@ -24,12 +26,10 @@ public class AsyncTask {
 	@Async("deactivateOTP_background_pool")
 	public void deactivateOTP() {
 		log.info("Starting Asynchronous job for deactivating old OTPs");
-		long initDeactivateOTP = System.currentTimeMillis();
+		long initDeactivateOTPTime = System.currentTimeMillis();
 		List<User> users = userRepository.findAllByUpdatedAtBeforeAndOneTimePasswordActive(LocalDateTime.now().minusMinutes(5), true);
-		for (User user : users) {
-			user.getOneTimePassword().setActive(false);
-		}
+		users.forEach(user -> user.getOneTimePassword().setActive(false));
 		userRepository.saveAll(users);
-		log.info("OTP Deactivation Job took: " + (System.currentTimeMillis() - initDeactivateOTP) + "ms");
+		log.info("OTP Deactivation Job took: " + (System.currentTimeMillis() - initDeactivateOTPTime) + "ms");
 	}
 }
